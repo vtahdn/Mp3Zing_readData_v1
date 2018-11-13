80874a60c12ed8057839b69ef53af082ee899cb6
Viet Asc <vtahdn@gmail.com>
2018-11-13 14:29:25 +0700

    Initial Commit

---
+++ Mp3Zing_readData_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mp3Zing_readData_v1
+//
+//  Created by Viet Asc on 11/13/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Mp3Zing_readData_v1Tests/Mp3Zing_readData_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Mp3Zing_readData_v1Tests.swift
+//  Mp3Zing_readData_v1Tests
+//
+//  Created by Viet Asc on 11/13/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import Mp3Zing_readData_v1
+
+class Mp3Zing_readData_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
